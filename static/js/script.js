ec69524642d68612043b65ae2fb6fb9bf252bb04
NixonValentino <Nicocoyo02@gmail.com>
2026-06-01 11:31:18 +0700

    hampir selesai!!!(landing pagenya doang)

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+function toggleFaq(el) {
+    const item = el.closest('.faq-item');
+    const isOpen = item.classList.contains('open');
+    document.querySelectorAll('.faq-item.open').forEach(i => i.classList.remove('open'));
+    if (!isOpen) item.classList.add('open');
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+
+  var TARGET_URL = 'koleksi.html';
+
+  var btnLoadMore = document.getElementById('btnLoadMore');
+
+  if (btnLoadMore) {
+    btnLoadMore.addEventListener('click', function () {
+      window.location.href = TARGET_URL;
+    });
+  }
+
+});
